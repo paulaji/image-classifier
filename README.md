3f23206e45532c2cdc6acbdb7a6f0e5ffbf75f5d
Paul Aji <paulajiparayil123@gmail.com>
2023-07-16 09:46:51 +0530

    Add files via upload
    completed the ML part

---
+++ README.md
@@ -0,0 +1,24 @@
+Uses Tenforflow's Keras library.
+This ML project is front-end supported (streamlit).
+
+Image can be given as input and will be classified into their own respective categories with the help of CIFAR-10.
+The CIFAR-10 dataset consists of images classified into 10 categories: airplane, automobile, bird, cat, deer, dog, frog, horse, ship, and truck.
+
+
+CIFAR-10:
+It is a predefined data set which helps in image classification.
+It is a well-known benchmark dataset that consists of 60,000 labeled images, specifically designed for image
+classification tasks. The CIFAR-10 dataset is widely used in the field of computer vision and serves as a standard
+benchmark for evaluating image classification algorithms and models.
+
+Weights in ML:
+In neural networks, which are commonly used in deep learning, the weights are associated with the connections between
+neurons in different layers. Each connection has a weight assigned to it, and these weights determine the influence of
+the input signals on the output of each neuron.
+If weight is close to 0, the input won't affect the output much.
+If weight is < 0 (negative), the increase in input will decrease the output!
+
+Adam (optimization algorithm):
+Adam (short for Adaptive Moment Estimation) is an optimization algorithm commonly used in machine learning, 
+particularly for training neural networks. It is an extension of the stochastic gradient descent (SGD) optimization 
+algorithm that combines elements of RMSprop and AdaGrad.
---
+++ main.py
@@ -0,0 +1,80 @@
+import numpy as np  # to work with arrays
+import matplotlib.pyplot as plt  # to plot
+import streamlit as st  # to build UI
+
+from PIL import Image  # to work with images
+
+# for the actual machine learning
+import tensorflow as tf
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+from keras.utils import to_categorical
+
+# 1) data preparation
+
+# loading data
+# This line loads the CIFAR-10 dataset, which consists of images classified into 10 different categories.
+# CIFAR-10 already has training data and test/validation data.
+# Therefore, the dataset is split into training data (X_train and y_train) and validation data (X_val and y_val).
+(X_train, y_train), (X_val, y_val) = cifar10.load_data()
+
+# preprocessing the data
+# breaking down rgb value of 255 in a range of 0 to 1 (pre-processing)
+# This line divides the training data by 255 to normalize the pixel values. By dividing by 255, the RGB values of each
+# pixel are rescaled to a range between 0 and 1. This normalization step ensures that all pixel values fall within the
+# same range, making it easier for the model to learn.
+X_train = X_train / 255
+X_val = X_val / 255
+
+# one hot encoding
+# In CIFAR-10,we usually classify the data into 10 categories and the labels might be integers ranging from 0 to 9.
+# One-hot encoding converts these integer labels into binary vectors of 0s and 1s.
+# One-hot encoding converts these integer labels into binary vectors of 0s and 1s.
+# By applying one-hot encoding, the model is prevented from assuming any inherent numerical relationship between the
+# classes. It eliminates any potential bias that could arise due to numerical representation, ensuring that the model
+# treats all classes equally and independently.
+y_train = to_categorical(y_train, 10)
+y_train = to_categorical(y_train, 10)
+
+# 2) model definition
+
+# creating a sequential model (neural network model as a sequence or a linear stack of layers)
+model = Sequential([
+    # first layer(input) - flat layer - flattens the input data into 32x32 (1024) image with 3 colour channels (rgb)
+    Flatten(input_shape=(32, 32, 3)),
+    # second layer(training) - dense layer - creating a neural network this 1000 neurons
+    Dense(1000, activation='relu'),
+    # third layer(output) - softmax and 10 are used, these indicate categorical classification (into 10 categories)
+    Dense(10, activation='softmax'),
+])
+
+# 3) model compilation
+
+# loss
+# 'categorical_crossentropy': This parameter specifies the loss function used during training.
+# For multi-class classification problems with integer labels (as in CIFAR-10),
+# the 'categorical_crossentropy' loss function is commonly used.
+
+# optimizer
+# This parameter determines the optimization algorithm used to update the model's weights during training.
+# The algorithm used here is the 'adam'.
+
+# metrics
+# These metrics provide quantitative insights into how well the model is performing on a given task,
+# such as classification, regression, or clustering.
+# ['accuracy']: Accuracy measures the proportion of correctly classified samples or predictions.
+# It is the ratio of correct predictions to the total number of predictions made.
+# Accuracy provides an overall measure of how well the model performs.
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 4) model training
+
+# batch_size=64:  This parameter determines the number of samples the model will process before updating its weights.
+# In this case, the model will process 64 samples.
+
+# epochs: This parameter specifies the number of times the model will iterate over the entire training data during the
+# training process (here, 10).
+model.fit(X_train, y_train, batch_size=64, epochs=10, validation_data=(X_val, y_val))
+
+model.save('cifar10_model.h5')
