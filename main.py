43753217be6ada75e6166f14d3bcd46d31d0574f
Paul Aji <paulajiparayil123@gmail.com>
2023-07-17 12:28:19 +0530

    streamlit part

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np  # to work with arrays
+import matplotlib.pyplot as plt  # to plot
+import streamlit as st  # to build UI
+import tensorflow as tf
+from PIL import Image  # to work with images
+
+st.title("CIFAR_10 Image Classifier")
+st.write("Upload an image and let the prediction happen!")
+
+
